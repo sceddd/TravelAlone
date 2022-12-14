46654b5095f9ef7493721557883bbc16019430bc
nppt <105217439+npptGithub@users.noreply.github.com>
2022-12-14 14:33:53 +0700

    des_location_detail

--- app/src/main/java/com/example/myapplication/database/DatabaseInf.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseInf.java
@@ -8,7 +8,7 @@ public class DatabaseInf {
         String username = "sceddd";
         String db = "TravelDBS";
         String port = "1433";
-        String ip = "172.16.12.138";        // switch this ip to your ip address
+        String ip = "192.168.3.128";        // switch this ip to your ip address
         this.url = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databasename=" + db + ";user=" + username + ";"+"password=" + password + ";";
     }
 
