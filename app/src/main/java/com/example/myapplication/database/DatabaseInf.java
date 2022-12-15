9040042720e20d76b2e354cfdc6aa4a987ab4f1a
nppt <105217439+npptGithub@users.noreply.github.com>
2022-12-15 17:08:01 +0700

    failed form

--- app/src/main/java/com/example/myapplication/database/DatabaseInf.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseInf.java
@@ -8,7 +8,7 @@ public class DatabaseInf {
         String username = "sceddd";
         String db = "TravelDBS";
         String port = "1433";
-        String ip = "192.168.3.47";        // switch this ip to your ip address
+        String ip = "192.168.3.41";        // switch this ip to your ip address
         this.url = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databasename=" + db + ";user=" + username + ";"+"password=" + password + ";";
     }
 
