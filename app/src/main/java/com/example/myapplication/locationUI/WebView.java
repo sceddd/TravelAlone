24e0b455f664b3cf30e654108fbd68fc59b234aa
nppt <105217439+npptGithub@users.noreply.github.com>
2022-12-16 12:22:22 +0700

    about us page

---
+++ app/src/main/java/com/example/myapplication/locationUI/WebView.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.locationUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class WebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
